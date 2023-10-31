244a5430da48e32e10624e14bdccaff12ab6e603
Ethan <ethan.gimmestad@icloud.com>
2023-10-30 21:11:00 -0600

    Initial application

--- Student/06/Superhero/hero/models.py
+++ Student/06/Superhero/hero/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Superhero(models.Model):
     name = models.CharField(max_length=200)
+    identity = models.CharField(max_length=100, default='')
     description = models.TextField()
+    strength = models.CharField(max_length=100, default='')
+    weakness = models.CharField(max_length=100, default='')
     image = models.CharField(max_length=200)
 
     def __str__(self):
